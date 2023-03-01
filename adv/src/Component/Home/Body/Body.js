8dec9295c364e9a11eed1629722e8af7ec07a112
zemelatt <zemelatmen@gmail.com>
2023-03-01 05:03:59 -0800

    shoping cart edit

--- adv/src/Component/Home/Body/Body.js
+++ adv/src/Component/Home/Body/Body.js
@@ -12,53 +12,53 @@ const Body = () => {
     <div>
       <div className="image-container">
         <div className="homequotes">
-          <div className="advice">
-            <h6>
-              Let's Make Life <div id="spin"></div>
-            </h6>
-          </div>
           <p className="quotes">
             “If we were meant to stay in one place, we’d have roots instead of
             feet.”
           </p>
-          <p>
+          {/* <p>
             <i className="quotesby">Rachel Wolchin</i>
-          </p>
+          </p> */}
+          <div className="advice">
+            <p>
+              Let's make life <div id="spin"></div>
+            </p>
+          </div>
         </div>
-        <div className="typeOfAdv">
-          <ul className="listOfType">
-            <li className="listAdv">
-              <SiSafari /> Safari
-            </li>
-            <li className="listAdv">
-              <GrYoga /> Yoga
-            </li>
-            <li className="listAdv">
-              <GiSummits /> Summer camps
-            </li>
-            <li className="listAdv">
-              <MdNordicWalking />
-              Trekking
-            </li>
+      </div>
+      <div className="typeOfAdv">
+        <ul className="listOfType">
+          <li className="listAdv">
+            <SiSafari /> Safari
+          </li>
+          <li className="listAdv">
+            <GrYoga /> Yoga
+          </li>
+          <li className="listAdv">
+            <GiSummits /> Summer camps
+          </li>
+          <li className="listAdv">
+            <MdNordicWalking />
+            Trekking
+          </li>
 
-            <li className="listAdv">
-              <BiCycling />
-              Cycling{" "}
-            </li>
-            <li className="listAdv">
-              <TbSpeedboat />
-              Canoeing
-            </li>
-            <li className="listAdv">
-              <MdOutlineDirectionsBoat />
-              Kayaking
-            </li>
-            <li className="listAdv">
-              <GiMountainClimbing />
-              Rock climbing
-            </li>
-          </ul>
-        </div>
+          <li className="listAdv">
+            <BiCycling />
+            Cycling{" "}
+          </li>
+          <li className="listAdv">
+            <TbSpeedboat />
+            Canoeing
+          </li>
+          <li className="listAdv">
+            <MdOutlineDirectionsBoat />
+            Kayaking
+          </li>
+          <li className="listAdv">
+            <GiMountainClimbing />
+            Rock climbing
+          </li>
+        </ul>
       </div>
     </div>
   );
--- server/db/db.js
+++ server/db/db.js
@@ -3,9 +3,8 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "",
   database: "adventure",
-  // socketPath: "/cloudsql/project:region:instance",
 });
 module.exports = db;
 // db.connect((err) => {
